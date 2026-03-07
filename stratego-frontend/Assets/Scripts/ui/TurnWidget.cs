e2131cfb491179269aa7470ac406c6cefa961a80
pjsillescas <pjsillescas@gmail.com>
2026-03-07 23:33:38 +0100

    Spinner desactivado esperando al contrario.

--- stratego-frontend/Assets/Scripts/ui/TurnWidget.cs
+++ stratego-frontend/Assets/Scripts/ui/TurnWidget.cs
@@ -5,9 +5,12 @@ public class TurnWidget : MonoBehaviour
 {
 	private const string YOUR_TURN = "Your turn";
 	private const string OPPONENT_TURN = "Opponent's turn";
+	private const string WAIT_FOR_OPPONENT = "Wait for your opponent";
 
 	[SerializeField]
 	private TextMeshProUGUI TurnText;
+	[SerializeField]
+	private WaitWidget WaitWidget;
 
 	private void OnEnable()
 	{
@@ -22,12 +25,22 @@ public class TurnWidget : MonoBehaviour
 	private void OnGameStateUpdated(object sender, GameStateDTO gameStateDTO)
 	{
 		TurnText.text = gameStateDTO.myTurn ? YOUR_TURN : OPPONENT_TURN;
+		/*
+		if (gameStateDTO.myTurn)
+		{
+			WaitWidget.Deactivate();
+		}
+		else
+		{
+			WaitWidget.Activate(WAIT_FOR_OPPONENT);
+		}
+		*/
 	}
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
 	{
-
+		WaitWidget.Deactivate();
 	}
 
 	// Update is called once per frame
